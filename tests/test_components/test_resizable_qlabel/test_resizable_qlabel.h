8a9e982e10e82019da801d91f533281a421af3a5
nikitamorozov <nikitwww@gmail.com>
2024-11-06 20:04:37 +0300

    test: add test for resizable_qlabel

---
+++ tests/test_components/test_resizable_qlabel/test_resizable_qlabel.h
@@ -0,0 +1,9 @@
+#include <QTest>
+
+class TestResizableQLabel : public QObject {
+    Q_OBJECT
+
+private slots:
+    void testPixmapScalingOnResize();
+};
+
---
+++ tests/test_ui/test_menu_window/menu_window_test.h
@@ -0,0 +1,9 @@
+#include <QTest>
+
+
+class TestMenuWindow : public QObject
+{
+    Q_OBJECT
+private slots:
+    
+};
