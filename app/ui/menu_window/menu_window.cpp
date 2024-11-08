986ada6b91af680c045dfff4068e0e30635960fa
Morozov Nikita <nikitwww@gmail.com>
2024-11-08 14:14:47 +0300

    Style clang format (#2)
    * feat: add main menu
    
    * test: add test for resizable_qlabel
    
    * ci: add build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * fix(menu_window): disconnect 1player button
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml and test linting
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: fix build.yml
    
    * ci: rename build to test
    
    * style(menu): clang-format fix

--- app/ui/menu_window/menu_window.cpp
+++ app/ui/menu_window/menu_window.cpp
@@ -1,42 +1,43 @@
 #include "menu_window.h"
 #include "components/resizable_qlabel/resizable_qlabel.h"
 
-MenuWindow::MenuWindow()
-{
-    this->setStyleSheet("background-color: black;");
-    this->setWindowTitle("Menu");
 
-    ResizableQLabel *imageLabel = new ResizableQLabel(this);
-    imageLabel->setPixmap(QPixmap(":/assets/ui/menu/logo.png"));
-    imageLabel->setAlignment(Qt::AlignCenter);
+MenuWindow::MenuWindow() {
+  this->setStyleSheet("background-color: black;");
+  this->setWindowTitle("Menu");
 
-    QPushButton *_1playerButton = new QPushButton("1 Player", this);
-    QPushButton *_2playersButton = new QPushButton("2 Players", this);
-    QPushButton *constructionButton = new QPushButton("Constructon", this);
-    //connect(_1playerButton, &QPushButton::clicked, qApp, &QCoreApplication::quit);
-    
-    int id = QFontDatabase::addApplicationFont(":/assets/fonts/Pixel Emulator.otf");
-    QString family = QFontDatabase::applicationFontFamilies(id).at(0);
-    QFont Buttonfont(family, 24);
+  ResizableQLabel *imageLabel = new ResizableQLabel(this);
+  imageLabel->setPixmap(QPixmap(":/assets/ui/menu/logo.png"));
+  imageLabel->setAlignment(Qt::AlignCenter);
 
+  QPushButton *_1playerButton = new QPushButton("1 Player", this);
+  QPushButton *_2playersButton = new QPushButton("2 Players", this);
+  QPushButton *constructionButton = new QPushButton("Constructon", this);
+  // connect(_1playerButton, &QPushButton::clicked, qApp,
+  // &QCoreApplication::quit);
 
-    _1playerButton->setFont(Buttonfont);
-    _2playersButton->setFont(Buttonfont);
-    constructionButton->setFont(Buttonfont);
+  int id =
+      QFontDatabase::addApplicationFont(":/assets/fonts/Pixel Emulator.otf");
+  QString family = QFontDatabase::applicationFontFamilies(id).at(0);
+  QFont Buttonfont(family, 24);
 
-     
-    QSpacerItem *leftSpacer = new QSpacerItem(100,100, QSizePolicy::Expanding, QSizePolicy::Expanding);
-    QSpacerItem *rightSpacer = new QSpacerItem(100,100, QSizePolicy::Expanding, QSizePolicy::Expanding);
+  _1playerButton->setFont(Buttonfont);
+  _2playersButton->setFont(Buttonfont);
+  constructionButton->setFont(Buttonfont);
 
+  QSpacerItem *leftSpacer =
+      new QSpacerItem(100, 100, QSizePolicy::Expanding, QSizePolicy::Expanding);
+  QSpacerItem *rightSpacer =
+      new QSpacerItem(100, 100, QSizePolicy::Expanding, QSizePolicy::Expanding);
 
-    QGridLayout *layout = new QGridLayout(this);
+  QGridLayout *layout = new QGridLayout(this);
 
-    layout->addWidget(imageLabel, 0, 0, 1, 3);
-    layout->addWidget(_1playerButton, 1, 1, Qt::AlignLeft);
-    layout->addWidget(_2playersButton, 2, 1, Qt::AlignLeft);
-    layout->addWidget(constructionButton, 3, 1, Qt::AlignLeft);
-    layout->addItem(leftSpacer, 1, 0, 2, 1);
-    layout->addItem(rightSpacer, 1, 2, 2, 1);
+  layout->addWidget(imageLabel, 0, 0, 1, 3);
+  layout->addWidget(_1playerButton, 1, 1, Qt::AlignLeft);
+  layout->addWidget(_2playersButton, 2, 1, Qt::AlignLeft);
+  layout->addWidget(constructionButton, 3, 1, Qt::AlignLeft);
+  layout->addItem(leftSpacer, 1, 0, 2, 1);
+  layout->addItem(rightSpacer, 1, 2, 2, 1);
 
-    qDebug() << "MenuWindow created";
+  qDebug() << "MenuWindow created";
 }
