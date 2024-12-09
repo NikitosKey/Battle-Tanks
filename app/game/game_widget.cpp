b7e4f4c2a75d49d8602300e90cd8296756ebc79f
Morozov Nikita <nikitwww@gmail.com>
2024-12-09 20:36:33 +0300

    feat: add empty game layout

---
+++ app/game/game_widget.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Никита Морозов on 19.11.2024.
+//
+
+#include "game_widget.h"
+
+namespace game {
+
+        GameWidget::GameWidget(QWidget *parent) : QWidget(parent) {
+            auto *layout = new QGridLayout;
+            this->setLayout(layout);
+            this->setStyleSheet("background-color: black;");
+            DEBUG_LOG("GameWidget created!");
+        }
+
+        GameWidget::~GameWidget() {
+            qDeleteAll(this->children());
+            DEBUG_LOG("GameWidget deleted!");
+        }
+}// namespace game
